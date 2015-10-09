1091afd9692a35f1842f4b15283bb003ac55be00
giorgioIamthedoctor <playclevergames@mail.ru>
2015-10-09 17:23:03 +0300

    Update A.py

--- A.py
+++ A.py
@@ -19,8 +19,10 @@
 inp = open("input.txt","r")
 wr = open("output.txt","w")
 s = list(inp.readlines())
-ns = list(map("int",s[1].split(" ")))
+ns = list(map(int,s[1].split(" ")))
 for i in range(int(s[0])):
   if ns.count(ns[i]) == 2:
-    print(ns[i])
+    wr.write(str(ns[i]))
+    wr.close()
+    inp.close()
     break
