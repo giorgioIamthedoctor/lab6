93ba3f5c4c38c471c4a330a135d8261f03efe663
giorgioIamthedoctor <playclevergames@mail.ru>
2015-10-09 17:33:48 +0300

    Update A.py

--- A.py
+++ A.py
@@ -26,3 +26,8 @@ for i in range(int(s[0])):
     wr.close()
     inp.close()
     break
+  if ns.count(ns[int(s[0])-1-i]) == 2:
+    wr.write(str(ns[int(s[0])-1-i]))
+    wr.close()
+    inp.close()
+    break
