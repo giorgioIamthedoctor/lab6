13748d6d17d9e5ff3bfc84b7327be49d9f124a2c
giorgioIamthedoctor <playclevergames@mail.ru>
2015-12-08 09:49:15 +0300

    Update D.py

--- D.py
+++ D.py
@@ -25,3 +25,17 @@
 | 6 4 6 3 |           |
 +---------+-----------+
 '''
+k=0
+l=0
+input=open('input.txt','r')
+output=open('output.txt','w')
+b=input.readline().split()
+k=int(b[0])
+n=int(b[1])
+a=input.readline().split()
+for i in range(k-1):
+    a1=input.readline().split()
+    for j in range(n):
+        a[j]=min(int(a1[j]),int(a[j]))
+for i in range(n):
+    print(a[i],end=' ',file=output)
