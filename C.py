ef9f960fae250c74f55129fdfb603cfe75420474
giorgioIamthedoctor <playclevergames@mail.ru>
2015-12-08 09:46:27 +0300

    Update C.py

--- C.py
+++ C.py
@@ -25,3 +25,22 @@
   | -40 41 -42 -41 42 40 |       |
   +----------------------+-------+
 '''
+input=open('input.txt', 'r')
+output= open('output.txt', 'w')
+N = int(input.readline())
+s = list(map(int, input.readline().split()))
+lmin = -1
+for i in range(N):
+    if s[i] > 0:
+        continue
+    itap = i
+    tap = s[i]
+    while itap + 1 < len(s):
+        itap += 1
+        if s[itap] == -tap:
+            if lmin == -1 or lmin > len(s[i:itap]):
+                lmin = len(s[i:itap])
+if lmin == -1:
+    print(0, file=output)
+else:
+    print(lmin, file=output)
